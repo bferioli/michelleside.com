ce6c3a0d479b55bc07d3064bcc59d9c43ce10037
Brandon Ferioli <bferioli@gmail.com>
2015-02-17 21:17:39 +0000

    Spaces for more pleasurable GitHub viewing

--- public/js/app.js
+++ public/js/app.js
@@ -4,46 +4,46 @@ define([
   'swipe',
   'fancybox'
 ], function($, Move, Swipe, Fancybox){
-	var App = {
-		dom: {},
-		fancyboxOptions: {
-			helpers:  {
-				overlay : {
-					css : {
-						'background' : 'transparent'
-					}
-				},
-				title : {
-					type : 'inside'
-				}
-			}
-		},
-		prepareGalleries: function() {
-			var self = this;
-			this.dom.galleries.each(function(i){
-				var gallery = $(this),
-					items = gallery.find('img');
+  var App = {
+    dom: {},
+    fancyboxOptions: {
+      helpers:  {
+        overlay : {
+          css : {
+            'background' : 'transparent'
+          }
+        },
+        title : {
+          type : 'inside'
+        }
+      }
+    },
+    prepareGalleries: function() {
+      var self = this;
+      this.dom.galleries.each(function(i){
+        var gallery = $(this),
+          items = gallery.find('img');
 
-				items.each(function(){
-					var url = this.src,
-						title = this.alt,
-						linkEl = $('<a rel="gallery' + i + '" href="' + url + '" title="' + title + '">' + this.outerHTML + '</a>');
+        items.each(function(){
+          var url = this.src,
+            title = this.alt,
+            linkEl = $('<a rel="gallery' + i + '" href="' + url + '" title="' + title + '">' + this.outerHTML + '</a>');
 
-					$(this).replaceWith(linkEl);
-				});
+          $(this).replaceWith(linkEl);
+        });
 
-				var links = gallery.find('a');
-				links.fancybox(self.fancyboxOptions);
-			});
-		},
-		initialize: function() {
-			this.dom.header = $('header');
-			this.dom.nav = $('nav#projects ul');
-			this.dom.galleries = this.dom.nav.find('.gallery');
+        var links = gallery.find('a');
+        links.fancybox(self.fancyboxOptions);
+      });
+    },
+    initialize: function() {
+      this.dom.header = $('header');
+      this.dom.nav = $('nav#projects ul');
+      this.dom.galleries = this.dom.nav.find('.gallery');
 
-			this.prepareGalleries();
-		}
-	};
+      this.prepareGalleries();
+    }
+  };
 
-	return App;
+  return App;
 });
